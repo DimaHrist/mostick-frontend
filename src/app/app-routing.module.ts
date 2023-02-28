dce104ba9be310954e273930f71f6284b0bfd3cc
d.khristyukha <d.khristyukha@gmail.com>
2023-03-01 01:33:15 +0300

    Fixed redirecting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ const routes: Routes = [
   //   loadChildren: () => import('src/app/modules/login/login.module').then((cm) => cm.LoginModule)
   // },
   {
+    path: '',
+    redirectTo: 'streams',
+    pathMatch: 'full'
+  },
+  {
     path: 'streams',
     loadChildren: () => import('src/app/modules/streams/streams.module').then((cm) => cm.StreamsModule)
   },
